fea9a81d1b035d11be483bac566ab6698d2fad83
Willian Nischiguti <williannischiguti@gmail.com>
2022-06-19 14:16:12 -0300

    Adicionados diversos comentários para efeitos de didática

--- src/desafios/desafio3.js
+++ src/desafios/desafio3.js
@@ -1,9 +1,19 @@
+/**
+    Nos casos de uso do document.getElemtentByID(), o elemento é possivelmente nulo e os ifs são necessários para
+    garantir que o código vai funcionar da melhor forma. A exclamação no final da declaração da variável botaoLimpar
+    indica que queremos dizer ao programa que aquele campo não é nulo.
+ */
 var botaoAtualizar = document.getElementById('atualizar-saldo');
 var botaoLimpar = document.getElementById('limpar-saldo');
-var soma = document.getElementById('soma');
+var soma = document.getElementById('soma'); // variável que irá receber o input do usuário
 var campoSaldo = document.getElementById('campo-saldo');
-var saldoTotal = 0;
-limparSaldo();
+var saldoTotal = 0; // variável que irá servir como acumulador
+limparSaldo(); // função que garante inicialmente que o saldo começa com 0 e também limpa o saldo, ou seja, volta para 0
+/**
+    A função somarAoSaldo irá acumular as somas na variável saldoTotal e a variável campoSaldo será convertida para
+    string e em seguida chamará a função limparCampoSoma() que faz com que, ao clicar no botão "Atualizar saldo", o
+    campo "Valor a ser adicionado:" (correspondente a variável "soma") seja limpo e possa receber novo valor.
+ */
 function somarAoSaldo(soma) {
     if (campoSaldo) {
         saldoTotal += soma;
@@ -22,9 +32,9 @@ function limparSaldo() {
 }
 if (botaoAtualizar) {
     botaoAtualizar.addEventListener('click', function () {
-        somarAoSaldo(Number(soma.value));
+        somarAoSaldo(Number(soma.value)); // chama a função somarAoSaldo e passa o valor (número) digitado pelo usuário.
     });
 }
 botaoLimpar.addEventListener('click', function () {
-    limparSaldo();
+    limparSaldo(); // chama a função limparSaldo que retorna o valor de saldo para 0.
 });
--- src/desafios/desafio3.ts
+++ src/desafios/desafio3.ts
@@ -1,12 +1,23 @@
+/**
+    Nos casos de uso do document.getElemtentByID(), o elemento é possivelmente nulo e os ifs são necessários para
+    garantir que o código vai funcionar da melhor forma. A exclamação no final da declaração da variável botaoLimpar
+    indica que queremos dizer ao programa que aquele campo não é nulo.
+ */
+
 let botaoAtualizar = document.getElementById('atualizar-saldo');
 let botaoLimpar = document.getElementById('limpar-saldo')!;
-let soma = document.getElementById('soma') as HTMLInputElement;
+let soma = document.getElementById('soma') as HTMLInputElement; // variável que irá receber o input do usuário
 let campoSaldo = document.getElementById('campo-saldo');
 
-let saldoTotal = 0
+let saldoTotal = 0 // variável que irá servir como acumulador
 
-limparSaldo()
+limparSaldo() // função que garante inicialmente que o saldo começa com 0 e também limpa o saldo, ou seja, volta para 0
 
+/**
+    A função somarAoSaldo irá acumular as somas na variável saldoTotal e a variável campoSaldo será convertida para
+    string e em seguida chamará a função limparCampoSoma() que faz com que, ao clicar no botão "Atualizar saldo", o
+    campo "Valor a ser adicionado:" (correspondente a variável "soma") seja limpo e possa receber novo valor.
+ */
 function somarAoSaldo(soma: number) {
     if (campoSaldo) {
         saldoTotal += soma
@@ -15,11 +26,11 @@ function somarAoSaldo(soma: number) {
     }
 }
 
-function limparCampoSoma() {
+function limparCampoSoma() { // limpa o campo soma para poder receber novo valor.
     soma.value = "";
 }
 
-function limparSaldo() {
+function limparSaldo() { // faz com que o saldo volte a ficar com valor 0.
     if (campoSaldo) {
         saldoTotal = 0;
         campoSaldo.innerHTML = saldoTotal.toString();
@@ -27,10 +38,10 @@ function limparSaldo() {
 }
 
 if (botaoAtualizar) {
-    botaoAtualizar.addEventListener('click', () => {
-        somarAoSaldo(Number(soma.value)); 
+    botaoAtualizar.addEventListener('click', () => { // ocorre no evento de clique.
+        somarAoSaldo(Number(soma.value)); // chama a função somarAoSaldo e passa o valor (número) digitado pelo usuário.
     });
 }
-botaoLimpar.addEventListener('click', () => {
-    limparSaldo();
+botaoLimpar.addEventListener('click', () => { // ocorre no evento de clique.
+    limparSaldo(); // chama a função limparSaldo que retorna o valor de saldo para 0.
 });
